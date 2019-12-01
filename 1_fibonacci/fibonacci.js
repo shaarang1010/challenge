fec7ef92243186343555bc2f6979481ac9c70955
shaarang <shaarang.1@hotmail.com>
2019-12-01 21:56:46 +1100

    adding my solutions for task1,2 and 3

--- 1_fibonacci/fibonacci.js
+++ 1_fibonacci/fibonacci.js
@@ -1,8 +1,69 @@
 // TODO: implement this function
+
+/*Couple of ways to implement this. 
+Option 1 - Using Iteration. Complexity - O(n)
+Option 2 - Binets formula for nth Fibonacci number (ref- http://www.maths.surrey.ac.uk/hosted-sites/R.Knott/Fibonacci/fibFormula.html). Complexity = O(1)
+*/
+
+const {performance} = require('perf_hooks');
+
+
 function fibonacci(n) {
-  throw new Error("Not implemented");
+  //throw new Error("Not implemented");
+  
+  // for default implementation, I have gone with option 2 as it has less time complexity.
+  return option2(n);
+
+  //for option1, uncomment this part
+  //return option1(n);
 }
 
-for (let i = 0; i < 10; i++) {
-  console.log(i, fibonacci(i));
+const option1 = (n) =>{
+  let [a,b,index] = [0,1,1] //initialize values for start, end and index
+
+  if(isNaN(n)) 
+    throw new Error('Not a number');
+
+  //if the n is less than equal to 1, return n
+  
+  if(n <= 1 ){
+    return n;
+  }
+  temp = 0;
+    while(index < n){
+      temp = a + b;
+      a = b;
+      b = temp;
+      index++; 
+    }
+    return temp;
 }
+
+
+const option2 = (n) =>{
+  /* using Binets formula for nth Fibonacci number in series 
+   
+   Fib(n) =  ([(1+sqrRoot(5))/2 - (1-sqrRoot(5))/2] / sqrRoot(5))
+   */
+  
+  let sqrtOf5 = Math.sqrt(5);
+
+  let positivePhi = (1+sqrtOf5)/2;
+  let negativePhi = (1-sqrtOf5)/2
+
+  return Math.round((Math.pow(positivePhi, n) - Math.pow(negativePhi, n)) / sqrtOf5);
+
+}
+
+
+function main(){
+  startTime = performance.now();
+  for (let i = 0; i < 10; i++) {
+    console.log(i, fibonacci(i));
+  }
+  endTime = performance.now();
+  console.log('Total Runtime for Option 2 = '+`${endTime - startTime}`+' ms');
+}
+
+main();
+
--- 2_async/async.js
+++ 2_async/async.js
@@ -5,8 +5,12 @@ const { resolve } = require("url");
 
 const SWAPI_URL = "https://swapi.co/api/";
 
-function apiGet(path, cb) {
-  https
+/* 
+apiGet returns a promise containing the data from path requested by the user
+*/
+function apiGet(path) {
+  return new Promise((onResolve, onReject) =>{
+    const request = https
     .get(
       resolve(SWAPI_URL, path),
       { headers: { Accept: "application/json" } },
@@ -18,94 +22,99 @@ function apiGet(path, cb) {
         });
 
         resp.on("end", () => {
-          cb(null, data ? JSON.parse(data) : null);
+          onResolve( data ? JSON.parse(data) : null); // send parsed json data
         });
       }
     )
-    .on("error", err => {
-      cb(err);
+    request.on("error", err => {
+      onReject(err); //
     });
-}
-
-function getPerson(query, cb) {
-  apiGet("people/?search=" + query, (err, data) => {
-    if (err) {
-      cb(err);
-      return;
-    }
-
-    const [person] = data.results;
-    cb(null, person);
   });
 }
 
-function readSearchTerms(cb) {
-  readFile(join(__dirname, "search.txt"), { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      cb(err);
-      return;
-    }
-    const nonEmptyLines = data.split("\n").filter(line => Boolean(line));
-    cb(null, nonEmptyLines);
-  });
+/* 
+getPerson takes in a character name and returns the data for the character (if any)
+*/
+
+function getPerson(query) {
+  return new Promise((onResolve, onReject)=>{
+    apiGet("people/?search=" + query)
+  .then((data) => {
+    const person = data.results;
+    onResolve(person);
+  })
+  .catch(err => onReject(err));
+  })
 }
 
-function saveNames(people, cb) {
+
+/* 
+Read file and split the data
+*/
+function readSearchTerms() {
+  return new Promise((onResolve, onReject) =>{
+
+    readFile(join(__dirname, "search.txt"), { encoding: "utf8" }, (err, data) => {
+  
+        const nonEmptyLines = data.split("\n").filter(line => Boolean(line))
+        
+        onResolve(nonEmptyLines);
+        onReject(err);
+    });
+  })
+  
+}
+
+// save names of characters. return promise once file is written
+
+function saveNames(people) {
   console.log(`Saving ${people.length} results`);
-  const data = people.map(p => (p ? p.name : "No results")).join("\n");
-  writeFile(join(__dirname, "names.txt"), data, { encoding: "utf8" }, cb);
+  return new Promise((onResolve, onReject) => {
+    data = people.map(p => (p ? p.name : "No results")).join("\n");
+    writeFile(join(__dirname, "names.txt"), data, { encoding: "utf8" }, (err) => {
+      if(err){
+        onReject(err);
+      } else {
+        onResolve("File Written Successfully!");
+      }
+    });
+  })
 }
+
+
+/*
+main program does the following - 
+1. readSearchTerms from file
+2. on succes, assign file content to terms 
+3. Map terms and get data for each character using getPerson function
+4. on success, push it to an empty array
+5. Write values to file using saveNames function. 
+*/
 
 function main() {
   // 1. Read the search terms in
   // 2. Run the searches in parallel
   // 3. Save the results to a file
   // NOTE: retain console output order
-
-  readSearchTerms((err, terms) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    // running multiple requests in parallel - keep count so we know when done
-    let numOk = 0;
-    let existingErr;
-    const people = Array(terms.length);
-
-    const personCallback = (err, data, i) => {
-      if (existingErr) {
-        // only care about the first error - ignore this
-        return;
-      }
-
-      if (err) {
-        existingErr = err;
-        console.error(err);
-        return;
-      }
-
-      people[i] = data;
-      numOk++;
-
-      if (numOk === terms.length) {
-        saveNames(people, err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log("Done");
-        });
-      }
-    };
-
-    terms.forEach((term, i) => {
-      getPerson(term, (err, data) => {
-        personCallback(err, data, i);
-      });
-    });
-  });
   console.log("Searching...");
+  
+  let terms='';
+  let people = [];
+   readSearchTerms()
+    .then((data)=>{
+      terms = data;
+      terms.map(async (term)=>{
+        await getPerson(term)
+        .then((person) =>{ 
+          people.push(person[0]);
+          if(people.length === terms.length){
+          saveNames(people)
+          .then(() =>console.log('Done'));
+      }
+        })
+      })
+  })
+    .catch(err => console.log(err));
 }
 
 main();
